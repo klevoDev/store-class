2220bfb43bd721f68df21e29d9e0697e4387ca58
Roman Lebedev <lr.mos@mail.ru>
2023-05-24 22:04:17 +0300

    add fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import {ShopItemClassComponent} from "./components/shop-item-class-component";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="App">
+        <div className="container">
+          <div className="background-element">
+          </div>
+          <div className="highlight-window">
+            <div className='highlight-overlay'></div>
+          </div>
+          <div className="window">
+            <ShopItemClassComponent />
+          </div>
+        </div>
+      </div>
+
   );
 }
 
